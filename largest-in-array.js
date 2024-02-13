2b135038503598b2735f78fb0915de470fbea420
SonamWangyel <stevenrex229@gmail.com>
2024-02-13 15:13:52 +0600

    Trying out new things

--- largest-in-array.js
+++ largest-in-array.js
@@ -1,14 +1,20 @@
-const numbers = [12, 44, 9.8473, 30, -12, 57382, 1, 0.3422]
-// var largestNum = numbers[0]
-// for (var i=0; i<numbers.length; i++){
-//     if (numbers[i] > largestNum) largestNum = numbers[i]  
-// }
-// console.log(largestNum)
+// const numbers = [12, 44, 9.8473, 30, -12, 57382, 1, 0.3422]
+// // var largestNum = numbers[0]
+// // for (var i=0; i<numbers.length; i++){
+// //     if (numbers[i] > largestNum) largestNum = numbers[i]  
+// // }
+// // console.log(largestNum)
 
 // console.log(Math.max(...numbers))
-numbers.sort(function(a, b) {
-    return b - a;
-});
+// // numbers.sort(function(a, b) {
+// //     return b - a;
+// // });
 
-// Displaying the modified array
-console.log(numbers);
+// // // Displaying the modified array
+// // console.log(numbers);    
+
+const alphabet = ['a','c','e','b','d']
+a = alphabet.sort()
+a.reverse()
+
+console.log(a)
