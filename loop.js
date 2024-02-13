1fde1c5e0294663135594e0e25dd03e17130b8d0
SonamWangyel <stevenrex229@gmail.com>
2024-02-13 11:36:04 +0600

    practice using loop (while loop)

--- loop.js
+++ loop.js
@@ -1,3 +1,17 @@
-for (var i=0; i <10; i++){
-    console.log(i)
+// for (var i=0; i <10; i++){
+//     console.log(i)
+// }
+
+var rl = require("readline-sync");
+
+var shouldLoopRun = true
+
+while (shouldLoopRun){
+    var reply = rl.question("Would you like to enter a name of a person?  ")
+    if (reply === "yes"){
+        n = rl.question("Enter the persons name: ")
+        shouldLoopRun = true
+    }else {
+        shouldLoopRun = false
+    }
 }
