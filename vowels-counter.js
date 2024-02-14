06230ca98733dc76d4dd47e6dd34fc211861587b
SonamWangyel <stevenrex229@gmail.com>
2024-02-14 15:57:54 +0600

    Created a program which counts the number of vowels in a word

---
+++ vowels-counter.js
@@ -0,0 +1,22 @@
+function vowelCounter(x) {
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    var counter = 0
+    var words = x.toLowerCase().split("") 
+
+    for (var i = 0; i < words.length; i++) {
+        for (var j = 0; j < vowels.length; j++) {
+            if (words[i] === vowels[j]) {
+                counter++;
+                break
+            }
+        }
+    }
+
+    if (counter > 0) {
+        return "The word has" + " " + counter + " " + "vowels";
+    } else {
+        return "There are no vowels in the given word";
+    }
+}
+
+console.log(vowelCounter("aeiou"));
