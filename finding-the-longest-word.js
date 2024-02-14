e63af7cdcbddd9ceea1822ed23b80e12d4eff39a
SonamWangyel <stevenrex229@gmail.com>
2024-02-14 13:49:21 +0600

    Modified it into function

--- finding-the-longest-word.js
+++ finding-the-longest-word.js
@@ -1,10 +1,11 @@
-var sentence = "I am Sonam Wangyel Dorji"
-var sentenceIntoWords = sentence.split(" ")
-var longestWord = ''
-for (var i=0 ; i < sentenceIntoWords.length; i++){
-     if (sentenceIntoWords[i].length > longestWord.length){
-        longestWord = sentenceIntoWords[i]
-        
-    } 
-}console.log(longestWord)
-
+function maxLengthOfWords(words){
+    var sentenceIntoWords = words.split(" ")
+    var longestWord = ''
+    for (var i=0 ; i < sentenceIntoWords.length; i++){
+         if (sentenceIntoWords[i].length > longestWord.length){
+            longestWord = sentenceIntoWords[i]
+        } 
+    }return longestWord
+     
+}
+console.log(maxLengthOfWords("Hi I Am Sonam Wangyel Dorji"))
