1c07dd6d38709d67808c32c9de3ca0a028ab7b04
SonamWangyel <stevenrex229@gmail.com>
2024-02-14 15:26:40 +0600

    Added a better solution to get century from year

--- Year-to-century-converter.js
+++ Year-to-century-converter.js
@@ -1,11 +1,34 @@
+var rl = require("readline-sync")
+x = rl.question("Insert a year")
 function yearToCenturyConverter(x){
     var year = Number(x);
     var century = ""
     if (year % 100 === 0 ){
-        century = year / 100 + " century"
+        century = year / 100 
+        if (century = 1 ){
+            century = century + "century"
+        }
     } else {
         century = Math.floor(year/100) + 1 + "century"
     }
     return century;
 }
-console.log(yearToCenturyConverter(2024))
+console.log(yearToCenturyConverter(x))
+// Best solution 
+// function getFromYear(year) {
+// 	const century = Math.ceil(year / 100)
+// 	var suffix = "th"
+// 	if (century % 100 >= 10 && century % 100 <= 20) {
+// 		suffix = "th"
+// 	} else if (century % 10 === 1) {
+// 		suffix = "st"
+// 	} else if (century % 10 === 2) {
+// 		suffix = "nd"
+// 	} else if (century % 10 === 3) {
+// 		suffix = "rd"
+// 	}
+
+// 	return century + suffix + " century"
+// }
+
+// console.log(getFromYear(1616)) // 17th
